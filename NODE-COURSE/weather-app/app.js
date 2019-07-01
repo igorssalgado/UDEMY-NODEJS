abe0930b7c77fde61ee2f4313bb9c43e6f63590f
Igor Salgado <igorssalgado@icloud.com>
2019-07-01 17:16:47 -0300

    error handling

--- NODE-COURSE/weather-app/app.js
+++ NODE-COURSE/weather-app/app.js
@@ -3,29 +3,35 @@ const request = require('request');
 url = 'https://api.darksky.net/forecast/53295cf6270e616f72bf4a2b3a7b42d6/-22.9845069,-47.0995991?units=si'; // -22.9845069,-47.0995991 home 
 
 request({ url: url, json: true }, (error, response) => {
-    // console.log(response.body);
-    const temp = response.body.currently.temperature;
-    const chance = response.body.currently.precipProbability;
-    const tempLow = response.body.daily.data[0].temperatureLow;
-    const tempHigh = response.body.daily.data[0].temperatureHigh
 
-    // const tempLowC = (tempLow - 32) * 5/9; // if tempLow comes in F
-    // const tempHighC = (tempHigh - 32) * 5/9; // if tempHigh comes in F
+    if (error) {
+        console.log("Unable to connect to weather service!");
+    } else if (response.body.error) {
+        console.log('unable to find location')
+    } else {
+        const temp = response.body.currently.temperature;
+        const chance = response.body.currently.precipProbability;
+        const tempLow = response.body.daily.data[0].temperatureLow;
+        const tempHigh = response.body.daily.data[0].temperatureHigh;
+    
+        console.log(response.body.daily.data[0].summary);
+        console.log("MIN: %sC MAX: %sC", tempLow.toFixed(1), tempHigh.toFixed(1));
+        console.log("It is currently %s Celsius degrees out. There is %s% chance of rain.", temp, chance);
+    }
 
-    console.log(response.body.daily.data[0].summary);
 
-    //F to C: (32 °F − 32) × 5/9 = 0 °C
+});
 
-    console.log("MIN: %s C MAX: %sC", tempLow.toFixed(1), tempHigh.toFixed(1));
-    console.log("It is currently %s Celsius degrees out. There is %s% chance of rain.", temp, chance )
-    // console.log("It is currently" + response.body.currently.temperature + " degrees out. There is" +  response.body.currently.precipProbability + "chance of rain.")
-})
+// const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Campinas.json?access_token=pk.eyJ1Ijoicm9naS0iLCJhIjoiY2p4a3J5M2JjMWYwZzNvcW5idGpvMDZ0ZyJ9.YHY19utjb5VluDgs0p2Fpg&limit=1";
 
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Campinas.json?access_token=pk.eyJ1Ijoicm9naS0iLCJhIjoiY2p4a3J5M2JjMWYwZzNvcW5idGpvMDZ0ZyJ9.YHY19utjb5VluDgs0p2Fpg&limit=1"
+// request({ url: geocodeURL, json: true }, (error, response) => {
 
-request({ url: geocodeURL, json: true }, (error, response) => {
-    const longitude = response.body.features[0].center[0]
-    const latitude = response.body.features[0].center[1]
-    console.log("Latitude: %s \nLongitude: %s", latitude, longitude)
-})
+//     if (error){
+//         console.log("Unable to connect to Geo Code service!");
+//     }else{
+//         const longitude = response.body.features[0].center[0];
+//         const latitude = response.body.features[0].center[1];
+//         console.log("Latitude: %s \nLongitude: %s", latitude, longitude);
+//     }
+// });
 
