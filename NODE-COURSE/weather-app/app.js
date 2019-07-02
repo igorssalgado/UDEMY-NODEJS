6a0f000e92aae2bdfac71af0f9718c434abcc102
Igor Salgado <igorssalgado@icloud.com>
2019-07-01 21:23:17 -0300

    fixing lat, long calling forecast

--- NODE-COURSE/weather-app/app.js
+++ NODE-COURSE/weather-app/app.js
@@ -1,13 +1,13 @@
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
-// geocode('campinas', (error, data) => {
-//     console.log('Error:', error);
-//     console.log('Data:', data)
-// });
+geocode('campinas', (error, data) => {
+    console.log('Error:', error);
+    console.log('Data:', data)
+});
 
 // order: lat, long
-forecast( '-22.9a056' , -47.0608, (error, data) => { 
+forecast( -22.9056 , -47.0608, (error, data) => { 
     console.log('Error:', error)
     console.log('Data:', data)
   })
