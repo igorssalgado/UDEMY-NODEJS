e4c249d9ed98d36d7cdbd17bdc64540407c0e6d2
Igor Salgado <igorssalgado@icloud.com>
2019-07-08 15:36:43 -0300

    removed console.log for latitude and longitude

--- NODE-COURSE/weather-app/utils/forecast.js
+++ NODE-COURSE/weather-app/utils/forecast.js
@@ -16,11 +16,10 @@ const forecast = (longitude, latitude, callback) => {
             const tempLow = response.body.daily.data[0].temperatureLow;
             const tempHigh = response.body.daily.data[0].temperatureHigh;
         
-            console.log('latiude: ' + response.body.latitude + '\nlongitude: ' + response.body.longitude); //to double check the location
             // console.log("MIN: %sC MAX: %sC", tempLow.toFixed(1), tempHigh.toFixed(1));
             // console.log("It is currently %s Celsius degrees out. There is %s% chance of rain.", temp, chance);
 
-            callback(undefined, '\n' + response.body.daily.data[0].summary + '\nMIN: ' + tempLow.toFixed(1) + 'C MAX: ' + tempHigh.toFixed(1) + 'C \nIt is currently ' + temp + ' Celsius degrees out. \nThere is ' + chance + '% chance of rain. ')
+            callback(undefined, '\n' + response.body.daily.data[0].summary + '\nMIN: ' + tempLow.toFixed(1) + 'C \nMAX: ' + tempHigh.toFixed(1) + 'C \n\nIt is currently ' + temp + ' Celsius degrees out. \nThere is ' + chance + '% chance of rain. ')
         }
     });
 }
