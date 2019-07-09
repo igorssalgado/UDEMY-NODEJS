8fd435e590c32a52b375712fc0897cdac8649551
Igor Salgado <igorssalgado@icloud.com>
2019-07-09 16:23:29 -0300

    Dynamic Pages with templating (HBS)

--- NODE-COURSE/web-server/src/app.js
+++ NODE-COURSE/web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather', 
+        name: 'Igor S'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me', 
+        name: 'Igor S'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help' , {
+        title: 'HELP!',
+        helpMsg: 'Helpful message.'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
