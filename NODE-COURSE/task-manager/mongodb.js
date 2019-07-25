a93af6f552af2a2477f2c571a1132a928bc18a60
Igor Salgado <igorssalgado@icloud.com>
2019-07-25 13:57:01 -0300

    inserting docs

--- NODE-COURSE/task-manager/mongodb.js
+++ NODE-COURSE/task-manager/mongodb.js
@@ -13,8 +13,53 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'Igor',
-        age: 27
+    // db.collection('users').insertOne({
+    //     name: 'Igor',
+    //     age: 27
+    // },(error, result) => {
+    //     if (error){
+    //         return console.log('Unable to insert user!');
+    //     }
+
+    //     console.log(result.ops);
+    //     console.log(result.insertedCount);
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     },
+    //     {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user!');
+    //     }
+        
+    //     console.log(result.ops);
+    //     // console.log(result.insertedCount);
+    //     // console.log(result.insertedIds)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: "description is a description",
+            completed: true
+        },{
+            description: "this description is another description",
+            completed: false
+        },{
+            description: "wow a new description which is a description",
+            completed: false
+        }
+    ], (error, result) => {
+            if (error){
+                return console.log("Unable to insert task!");
+            }
+
+            console.log(result.ops);
     })
 })
