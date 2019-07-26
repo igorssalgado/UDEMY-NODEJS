1ec3325f4d66da8e17d907934839038d0c3b28c3
Igor Salgado <igorssalgado@icloud.com>
2019-07-26 04:54:30 -0300

    mongoose model

--- NODE-COURSE/task-manager/src/db/mongoose.js
+++ NODE-COURSE/task-manager/src/db/mongoose.js
@@ -15,14 +15,33 @@ const User = mongoose.model('User', {
    }
 });
 
-const me = new User({
-    name: 'Igor',
-    age: 27
+const Task = mongoose.model('Task', {
+    descripion: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
 });
 
+const task = new Task({
+    descripion: "feed the dogs",
+    completed: true
+});
 
-me.save().then(() => {
-    console.log(me);
-}).catch((error) =>{
-    console.log('Error!', error)
-})
+task.save().then(() => {
+    console.log(task);
+}).catch((error) => {
+    console.log('Error to save task!', error)
+});
+
+// const me = new User({
+//     name: 'Igor',
+//     age: 27
+// });
+
+// me.save().then(() => {
+//     console.log(me);
+// }).catch((error) =>{
+//     console.log('Error!', error)
+// })
