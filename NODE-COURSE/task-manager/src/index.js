dc02f2a1d227dc863879b82452361b95c36b4571
Igor Salgado <igorssalgado@icloud.com>
2019-07-29 16:22:57 -0300

    Resource Reading Endpoints

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -8,6 +8,51 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(201).send(users);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id;
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }).catch((e) => {
+        res.status(500).send();
+    });
+
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.status(201).send(tasks);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id;
+
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    }).catch((e) => {
+        res.status(500).send();
+    })
+})
+
 app.post('/users', (req, res) => {
     const user = new User(req.body);
 
@@ -21,13 +66,13 @@ app.post('/users', (req, res) => {
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
-    task.save().then(() =>{
+    task.save().then(() => {
         res.status(201).send(task);
     }).catch((e) => {
         res.status(400).send(e);
     })
-})
+});
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
-})
+});
