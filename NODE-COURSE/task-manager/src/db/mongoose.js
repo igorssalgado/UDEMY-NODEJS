e6da223c49dc4ebcbb981d9455a58e2774fa85b0
Igor Salgado <igorssalgado@icloud.com>
2019-07-29 15:23:54 -0300

    resourse creation part I

--- NODE-COURSE/task-manager/src/db/mongoose.js
+++ NODE-COURSE/task-manager/src/db/mongoose.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,45 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 });
 
 // Model
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes("password")) {
-                throw new Error("Password should not contains 'password' word");
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    }
-});
 
 const Task = mongoose.model('Task', {
     descripion: {
@@ -60,31 +20,5 @@ const Task = mongoose.model('Task', {
 });
 // Model
 
-// Method calls
-
-const task = new Task({
-    descripion: " feed the dogs again         ",
-    completed: true
-});
-
-task.save().then(() => {
-    console.log(task);
-}).catch((error) => {
-    console.log('Error to save task!', error)
-});
-
-// const me = new User({
-//     name: '   Igor  ',
-//     email: ' myemaiL@gmail.coM   ',
-//     password: 'testingpassw0rd'
-// });
-
-// me.save().then(() => {
-//     console.log(me);
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 
-
-// Method calls
