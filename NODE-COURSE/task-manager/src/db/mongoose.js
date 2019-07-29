80ed566531bc07ec3a09edd878e05179b3d38ae4
Igor Salgado <igorssalgado@icloud.com>
2019-07-29 15:44:50 -0300

    Resource Creation Endpoints: Part II

--- NODE-COURSE/task-manager/src/db/mongoose.js
+++ NODE-COURSE/task-manager/src/db/mongoose.js
@@ -5,20 +5,4 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 });
 
-// Model
-
-const Task = mongoose.model('Task', {
-    descripion: {
-        type: String,
-        requried: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
-// Model
-
-
 
--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user')
+const User = require('./models/user');
+const Task = require('./models/task');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/users', (req, res) => {
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     user.save().then(() => {
         res.send(user);
@@ -16,6 +17,16 @@ app.post('/users', (req, res) => {
         res.status(400).send(e);
     })
 });
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() =>{
+        res.send(task);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
