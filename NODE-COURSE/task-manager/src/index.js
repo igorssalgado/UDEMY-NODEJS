f05dd7f939e48e973eaa15780c9455d0f135e7c4
Igor Salgado <igorssalgado@icloud.com>
2019-07-30 17:04:12 -0300

    Resource Deleting Endpoints

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -66,6 +66,21 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user)
+
+    } catch (e) {
+        res.status(500).send(e);
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body);
 
@@ -91,6 +106,20 @@ app.get('/tasks/:id', async (req, res) => {
 
     try {
         const task = await Task.findById(_id);
+        if (!task) {
+            return res.status(404).send();
+        }
+
+        res.send(task);
+    } catch (e) {
+        res.status(500).send();
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        
         if (!task) {
             return res.status(404).send();
         }
