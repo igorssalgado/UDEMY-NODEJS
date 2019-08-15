6d6df817c1cf6001fcecf39cd0d271375b5cb32d
Igor Salgado <47892709+igorssalgado@users.noreply.github.com>
2019-08-15 09:55:20 -0400

    middleware

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -6,6 +6,19 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     if (req.method === 'GET') {
+//         res.send('Get requests are disable');
+//     } else {
+//         next();
+//     }
+// });
+
+//Middleware for Maint mode
+app.use((req, res, next) => {
+    res.status(503).send('Server is currently down. check later.');
+});
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
