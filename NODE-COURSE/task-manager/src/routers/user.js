ecc3a8e3fecd58c716609aa127b3ec0c93935651
Igor Salgado <igorssalgado@icloud.com>
2019-08-14 21:26:19 -0300

    creating token

--- NODE-COURSE/task-manager/src/routers/user.js
+++ NODE-COURSE/task-manager/src/routers/user.js
@@ -3,17 +3,31 @@ const User = require('../models/user')
 
 const router = new express.Router();
 
+//to create a new user
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
 
     try {
         await user.save();
-        res.status(201).send(user);
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token});
     } catch (e) {
         res.status(400).send(e);
     }
 });
 
+//to login with a user and gerating a session token
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken()
+        res.send({ user, token });
+    } catch (e) {
+        res.status(400).send();
+    }
+})
+
+//to find all users
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({});
@@ -23,6 +37,7 @@ router.get('/users', async (req, res) => {
     }
 })
 
+//to find a user by id
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id;
 
@@ -39,6 +54,7 @@ router.get('/users/:id', async (req, res) => {
     }
 });
 
+//to update a user by id
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['name', 'email', 'password', 'age'];
@@ -66,15 +82,7 @@ router.patch('/users/:id', async (req, res) => {
     }
 })
 
-router.post('/users/login', async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        res.send(user);
-    } catch (e) {
-        res.status(400).send();
-    }
-})
-
+//to delete a user by id
 router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
