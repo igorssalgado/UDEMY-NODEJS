4fe719ab4a493f296c24970b8a13cc2213c45164
Igor Salgado <igorssalgado@icloud.com>
2019-08-16 23:16:24 -0300

    Authenticating Task Endpoints

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -27,18 +27,18 @@ app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
 
-const Task = require('./models/task');
-const User = require('./models/user');
+// const Task = require('./models/task');
+// const User = require('./models/user');
 
-const main = async () => {
-    // const task = await Task.findById('5d5754c6e92d4520940f975e');
-    // await task.populate('owner').execPopulate() //it is gonna 'go off', find the user associate with this task and task.owner will be their profile the entire document opose of just being the id
-    // console.log(task.owner);
+// const main = async () => {
+//     // const task = await Task.findById('5d5754c6e92d4520940f975e');
+//     // await task.populate('owner').execPopulate() //it is gonna 'go off', find the user associate with this task and task.owner will be their profile the entire document opose of just being the id
+//     // console.log(task.owner);
 
-    const user = await User.findById('5d5750d70e2a2701bcc78ece');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
-    console.log(user.name);
-}  
+//     const user = await User.findById('5d575df855b31004f050f29b');
+//     await user.populate('tasks').execPopulate();
+//     console.log(user.tasks);
+//     console.log(user.name);
+// }  
 
-main();
+// main();
