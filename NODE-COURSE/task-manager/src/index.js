a496fd59a5ac57360665855c2e070a73f8eba155
IGOR DOS SANTOS SALGADO <isantos@br.ibm.com>
2019-08-19 10:41:51 -0400

    multer (file upload)

--- NODE-COURSE/task-manager/src/index.js
+++ NODE-COURSE/task-manager/src/index.js
@@ -6,18 +6,13 @@ const taskRouter = require('./routers/task')
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('Get requests are disable');
-//     } else {
-//         next();
-//     }
-// });
-
-//Middleware for Maint mode
-// app.use((req, res, next) => {
-//     res.status(503).send('Server is currently down. check later.');
-// });
+const multer = require('multer');
+const upload = multer({
+    dest: 'images'
+});
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send();
+})
 
 app.use(express.json());
 app.use(userRouter);
@@ -26,19 +21,3 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//     // const task = await Task.findById('5d5754c6e92d4520940f975e');
-//     // await task.populate('owner').execPopulate() //it is gonna 'go off', find the user associate with this task and task.owner will be their profile the entire document opose of just being the id
-//     // console.log(task.owner);
-
-//     const user = await User.findById('5d575df855b31004f050f29b');
-//     await user.populate('tasks').execPopulate();
-//     console.log(user.tasks);
-//     console.log(user.name);
-// }  
-
-// main();
